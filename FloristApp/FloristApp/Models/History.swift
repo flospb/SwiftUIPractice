ae0a7b395a1c5eda261d77b124b2c2ea28d6d7cf
Serg <73400440+flospb@users.noreply.github.com>
2023-03-31 11:22:34 +0300

    Add practice project
    Chapter 1-5

---
+++ FloristApp/FloristApp/Models/History.swift
@@ -0,0 +1,22 @@
+//
+//  History.swift
+//  FloristApp
+//
+//  Created by florist on 30.03.2023.
+//
+
+import Foundation
+
+struct History: Identifiable, Codable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    var lengthInMinutes: Int
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int = 5) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+    }
+}
---
+++ FloristApp/FloristApp/Models/TrailingIconLabelStyle.swift
@@ -0,0 +1,22 @@
+//
+//  TrailingIconLabelStyle.swift
+//  FloristApp
+//
+//  Created by florist on 29.03.2023.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
