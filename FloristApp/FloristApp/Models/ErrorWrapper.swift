91d22478771edf2892b4f3a89bf29485b7cf956a
Serg <73400440+flospb@users.noreply.github.com>
2023-04-03 15:59:18 +0300

    Завершены последние главы проекта от Apple

---
+++ FloristApp/FloristApp/Models/ErrorWrapper.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorWrapper.swift
+//  FloristApp
+//
+//  Created by florist on 31.03.2023.
+//
+
+import Foundation
+
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+
+    init(id: UUID = UUID(), error: Error, guidance: String) {
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+}
